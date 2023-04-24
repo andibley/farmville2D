0aa7b979b4a5e0126a5e58ee22a13ad914b3c21c
andibley <andi_bley25@milton.edu>
2023-04-24 14:07:01 -0400

    work during class on 4/24 added 2d array

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -2,9 +2,20 @@ public class MyFarm {
 
     public static void main(String[] args) {
         MyFarm charlie = new MyFarm();
+        plot ryan = new plot();
+        ryan.printPlot();
     }
 
-    public MyFarm(){
-      System.out.print("hello from my farm!");
+    public MyFarm() {
+        System.out.print("hello from my farm!");
+
+
+        plot[][] grid = new plot[10][10];
+        for (int p = 0; p < grid.length; p++) {
+            for (int h = 0; h < grid[p].length; h++) {
+                grid[p][h] = new plot();
+                grid[p][h].printPlot();
+            }
+        }
     }
 }
--- src/plot.java
+++ src/plot.java
@@ -3,16 +3,31 @@ public class plot {
    public int numberofplants;
    public boolean needswater;
 
+
    public plot() {
-      // plantname = "sunflower";
-      needswater = true;
-     int num = (int) (Math.random() * 5);
-     
+      needswater = false;
+     numberofplants = (int)(Math.random() * 100) +10;
+int num = (int)(Math.random()*5);
       if (num == 0) {
          plantname = "sunflower";
       }
       else if(num == 1){
             plantname = "corn";
          }
+      else if(num ==2){
+          plantname = "carrot";
+      }
+      else if(num == 3){
+          plantname = "tomato";
+      }
+      else{
+          plantname = "empty";
+          numberofplants =0;
+      }
+      }
+
+      public void printPlot(){
+System.out.println("the plot has " + numberofplants + " of "+ plantname+ " and it is "+ needswater+ " that it needs water.");
+
       }
    }
