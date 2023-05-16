638bc7d49d27cc5e053998c5ef85f82a3581f23f
andibley <andi_bley25@milton.edu>
2023-05-16 11:23:52 -0400

    class work 5/16

--- src/MyFarmArraylist.java
+++ src/MyFarmArraylist.java
@@ -5,14 +5,27 @@ public class MyFarmArraylist {
 
     public MyFarmArraylist() {
         System.out.println("my farm using array lists");
-
-        for (int p = 0; p < 10; p++) {
-
+        for(int p = 0; p < 10; p++) {
             System.out.println(row.size());
             row.add(new plot());
             row.get(p).printPlot();
+
         }
-            row.add(new plot());
         System.out.println(row.size());
-        }
+        totalPlants();
     }
+        public void totalPlants() {
+            // how many total plants are there in the row?
+            int total = 0;
+        for(plot a: row){
+            total+= a.numberofplants;
+        }
+        System.out.println("total plants is "+total);
+        }
+
+
+    }
+
+
+
+
