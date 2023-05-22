3b86efcc2789ff6942702e08f56721d2f4fc3062
andibley <andi_bley25@milton.edu>
2023-05-22 14:12:59 -0400

    class 5/22

--- src/MyFarmArraylist.java
+++ src/MyFarmArraylist.java
@@ -5,7 +5,7 @@ public class MyFarmArraylist {
 
     public MyFarmArraylist() {
         System.out.println("my farm using array lists");
-        for(int p = 0; p < 10; p++) {
+        for (int p = 0; p < 10; p++) {
             System.out.println(row.size());
             row.add(new plot());
             row.get(p).printPlot();
@@ -13,18 +13,79 @@ public class MyFarmArraylist {
         }
         System.out.println(row.size());
         totalPlants();
+        addSunflower();
+        addcorn();
+        printPlantName();
+        tomatolocations();
+        totalcarrots();
+        averageNumberOfPlants();
     }
-        public void totalPlants() {
-            // how many total plants are there in the row?
-            int total = 0;
-        for(plot a: row){
-            total+= a.numberofplants;
-        }
-        System.out.println("total plants is "+total);
-        }
 
 
+    public void printPlantName() {
+        for (int k = 0; k < row.size(); k++) {
+            System.out.print(k + ":" + row.get(k).plantname + "\t");
+        }
+        System.out.println(" ");
     }
+
+    public void totalPlants() {
+        // how many total plants are there in the row?
+        int total = 0;
+        for (plot a : row) {
+            total += a.numberofplants;
+        }
+        System.out.println("total plants is " + total);
+    }
+
+    public void addSunflower() {
+        plot sunflower = new plot();
+        sunflower.plantname = "sunflower";
+        sunflower.numberofplants = 42;
+        sunflower.needswater = true;
+
+        sunflower.printPlot();
+        row.add(sunflower);
+
+    }
+
+    public void addcorn() {
+        plot corn = new plot();
+        corn.plantname = "corn";
+        corn.numberofplants = 70;
+        corn.needswater = false;
+        corn.printPlot();
+        row.add(3, corn);
+
+    }
+
+    public void tomatolocations() {
+        for (int m = 0; m < row.size(); m++) {
+            if (row.get(m).plantname.equals("tomato"))
+                System.out.println(m);
+        }
+    }
+
+    public void totalcarrots(){
+       int numberofcarrots = 0;
+        for(plot b: row){
+            if(b.plantname.equals("carrot"))
+            numberofcarrots += b.numberofplants;
+        }
+        System.out.println("total number of carrots is " + numberofcarrots);
+    }
+
+    public void averageNumberOfPlants() {
+int averagenumberofplants = 0;
+for(plot d: row){
+    averagenumberofplants += d.numberofplants;
+
+}
+    }
+    }
+
+
+
 
 
 
